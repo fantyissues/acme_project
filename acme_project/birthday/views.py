1f97f84a66b169e6bd9131361ab67a337e3a1baa
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-02 11:51:47 +0500

    Добавить пагинатор для страницы birthday/list/

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import BirthdayForm
@@ -36,6 +37,9 @@ def delete_birthday(request, pk):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.order_by('birthday')
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
