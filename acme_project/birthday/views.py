023996b173a98063d6473b1e3bc575e2a3bbe178
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-02 15:50:08 +0500

    Добавить BirthdayMixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -7,24 +7,25 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
+class BirthdayMixin:
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
 
 
-class BirthdayCreateView(CreateView):
-    model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    pass
 
 
-class BirthdayUpdateView(UpdateView):
-    model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    pass
 
 
 def birthday(request, pk=None):
