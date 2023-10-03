c7c34452454d7c0e35892720d2539e7b5681f06b
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-03 21:48:50 +0500

    Настроить сохранение отправленных писем в sent_emails/

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -91,3 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
