c89423b37064ff025da1880b2dab76a263dd2577
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-06 15:28:29 +0500

    Добавить возможность изменять и удалть записи только автором

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
+from django.shortcuts import get_object_or_404
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayUpdateDeleteMixin:
+    model = Birthday
+
+    def dispatch(self, request, *args, **kwargs):
+        instance = get_object_or_404(Birthday, pk=kwargs['pk'])
+        if instance.author != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
 
 
 class BirthdayListView(ListView):
@@ -18,14 +30,18 @@ class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
-    model = Birthday
+
+class BirthdayUpdateView(LoginRequiredMixin, BirthdayUpdateDeleteMixin,
+                         UpdateView):
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
-    model = Birthday
+class BirthdayDeleteView(LoginRequiredMixin, BirthdayUpdateDeleteMixin,
+                         DeleteView):
     success_url = reverse_lazy('birthday:list')
 
 
