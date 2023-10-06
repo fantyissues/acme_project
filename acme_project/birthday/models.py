d47e17e2f53305c8d84cba51563f45a825bcf7ae
Denis Lopin <denis.lopin.w@yandex.ru>
2023-10-06 23:44:01 +0500

    Добавить модель Tag и вывести теги на страницу со списком дней рождения

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -7,6 +7,12 @@ from .validators import real_age
 User = get_user_model()
 
 
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    def __str__(self):
+        return self.tag
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
@@ -19,6 +25,12 @@ class Birthday(models.Model):
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
     author = models.ForeignKey(
         User, verbose_name='Автор', on_delete=models.CASCADE, null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
     )
 
     def get_absolute_url(self):
